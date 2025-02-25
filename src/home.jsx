7b50393d4c3280718daeaf9b82b1874330453899
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-02-25 14:55:28 +0530

    Completed Assignmnet

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Vraj Patel</h1>
+      <p>Contact:- vrajpatel402126@gmail.com</p>
+      <p>Phone No:- 848595XXXX </p>
+    </div>
+  );
+};
+
+export default HomePage;
